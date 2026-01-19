d0eb6d767b5b1f8783d1d7059281d6878358a69d
Hozz1 <dmitriylazebnyy911@gmail.com>
2026-01-20 02:51:20 +0300

    admin

--- backend/poetry/admin.py
+++ backend/poetry/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Poetry
+from .models import Poetry, Language
 
 # Register your models here.
 admin.site.register(Poetry)
+admin.site.register(Language)
