a8170c83ff60d2162f9372a4d9983cada15e153b
Hozz1 <dmitriylazebnyy911@gmail.com>
2026-01-20 02:59:50 +0300

    admin

--- backend/poetry/admin.py
+++ backend/poetry/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-
 from .models import Poetry, Language
 
-# Register your models here.
-admin.site.register(Poetry)
-admin.site.register(Language)
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ("id", "language")
+    search_fields = ("language",)
+
+@admin.register(Poetry)
+class PoetryAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "language", "time_create")
+    search_fields = ("title", "description", "text")
+    list_filter = ("language",)
